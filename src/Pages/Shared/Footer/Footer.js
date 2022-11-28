43d54f4a06308409572c87a2f1a157ae7daee831
sawroar <sawroarhossain5@gmail.com>
2022-11-28 13:58:34 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import logo from './../../../Assets/logo.png'
 const Footer = () => {
     return (
-        <div>
-            <h1>This is Footer</h1>
-        </div>
+        <footer className="footer p-10 bg-base-200 text-base-content">
+            <div>
+                <img src={logo} alt="" height={'300px'} width={'300px'} />
+                <p>Carway Industries.<br />Providing reliable car manufacturing services  since 1992</p>
+            </div>
+            <div>
+                <span className="footer-title">Services</span>
+                <a href='/' className="link link-hover">Branding</a>
+                <a href='/' className="link link-hover">Design</a>
+                <a href='/' className="link link-hover">Marketing</a>
+                <a href='/' className="link link-hover">Advertisement</a>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a href='/' className="link link-hover">About us</a>
+                <a href='/' className="link link-hover">Contact</a>
+                <a href='/' className="link link-hover">Jobs</a>
+                <a href='/' className="link link-hover">Press kit</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a href='/' className="link link-hover">Terms of use</a>
+                <a href='/' className="link link-hover">Privacy policy</a>
+                <a href='/' className="link link-hover">Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 

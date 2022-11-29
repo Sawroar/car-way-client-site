ccf2d1ee85a11e8b8b41ec84a96183de466e5b13
sawroar <sawroarhossain5@gmail.com>
2022-11-29 13:41:42 +0600

    google signin method created

--- src/Pages/Shared/ErrorPage/ErrorPage.js
+++ src/Pages/Shared/ErrorPage/ErrorPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import errorimg from './../../../Assets/kec2_cpyi_210528.jpg'
 const ErrorPage = () => {
     return (
@@ -6,7 +7,10 @@ const ErrorPage = () => {
             <div>
 
                 <img src={errorimg} height={'500px'} width={'500px'} alt="" />
-                <p className='text-center text-4xl text-red-600'>404! Opps Wrong Routes</p>
+                <p className='text-center text-4xl text-red-600 mb-4'>404! Opps Wrong Routes. </p>
+                <p className=' text-center'>
+                    <Link to='/' className='text-xl text-blue-600'>Go to Home</Link>
+                </p>
             </div>
         </div>
     );
